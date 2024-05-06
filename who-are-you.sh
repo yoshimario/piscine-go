793d3f04982edb034d404755c3f39663639b1f50
skipina <paintballsean@gmail.com>
2024-05-06 21:53:54 +0300

    Added who are you update

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # Fetch superhero data and filter by ID 70
-name=$(curl -s https://01.gritlab.ax/assets/superhero/all.json | jq -r '.[] | select(.id == 70) | .name')
-
-# Output the name
-echo "\"name\"$name"
+curl -s https://01.gritlab.ax/assets/superhero/all.json | jq '.[] | select ( .id == 70 )' | jq .name
