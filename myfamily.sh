6c32f899c4159484fa80ba38ae115987ba238b75
skipina <paintballsean@gmail.com>
2024-05-06 22:43:49 +0300

    myfamily update

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s  https://01.gritlab.ax/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" -r '.[] | select(.id == ($HERO_ID|tonumber)) | .connections.relatives' | tr -d '"'
+curl -s  https://01.gritlab.ax/assets/superhero/all.json | jq ".[] | select(.id ==$HERO_ID) | .connections.relatives " | sed -e 's/^"//' -e 's/"$//'
