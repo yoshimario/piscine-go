c7f3282fee69d44ab9e6b7b3036fa92dd396daae
skipina <paintballsean@gmail.com>
2024-05-06 22:41:10 +0300

    fixe myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export HERO_ID=1; curl -s  https://01.gritlab.ax/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" -r '.[] | select(.id == ($HERO_ID|tonumber)) | .connections.relatives' | tr -d '"'
+curl -s  https://01.gritlab.ax/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" -r '.[] | select(.id == ($HERO_ID|tonumber)) | .connections.relatives' | tr -d '"'
