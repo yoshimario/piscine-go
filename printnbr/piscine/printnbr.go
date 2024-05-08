12a580c7e6a8cf58443539acccb8c159b8538e9e
skipina <paintballsean@gmail.com>
2024-05-08 23:29:25 +0300

    added printnbr.go

---
+++ printnbr/piscine/printnbr.go
@@ -0,0 +1,14 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n *= -1
+	}
+	if n >= 10 {
+		PrintNbr(n / 10)
+	}
+	z01.PrintRune(rune('0' + n%10))
+}
