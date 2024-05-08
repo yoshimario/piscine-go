60bd38e3844117f4c29138e1ebd4a0c1e94b7e26
skipina <paintballsean@gmail.com>
2024-05-08 23:33:34 +0300

    added new file code to root

--- printnbr.go
+++ printnbr.go
@@ -3,6 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		n = 223372036854775808 // Add 1 to make it positive
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n *= -1
