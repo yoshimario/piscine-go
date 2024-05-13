910209210804b7174724f294d1f147bbb091a1bc
skipina <paintballsean@gmail.com>
2024-05-13 14:10:24 +0300

    changed function

--- Quest-04/iterativefactorial/piscine/iterativefactorial.go
+++ Quest-04/iterativefactorial/piscine/iterativefactorial.go
@@ -1,18 +1,16 @@
-package piscine
-
-func IterativeFactorial(nb int) int {
+func IterativeFactorial(nb int) int64 {
 	// Factorial of negative numbers or values greater than 12 will overflow int
 	if nb < 0 || nb > 12 {
-		return 0
+			return 0
 	}
 
-	result := 1
+	result := int64(1)
 	for i := 1; i <= nb; i++ {
-		result *= i
-		// Check for overflow
-		if result <= 0 {
-			return 0
-		}
+			result *= int64(i)
+			// Check for overflow
+			if result <= 0 {
+					return 0
+			}
 	}
 
 	return result
