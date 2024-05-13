2b4efd182e131cfa5cf5b8839ad98493980b4207
skipina <paintballsean@gmail.com>
2024-05-13 14:05:00 +0300

    gofumpt error

--- Quest-04/iterativefactorial/piscine/iterativefactorial.go
+++ Quest-04/iterativefactorial/piscine/iterativefactorial.go
@@ -1,19 +1,19 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-		// Factorial of negative numbers or values greater than 12 will overflow int
-		if nb < 0 || nb > 12 {
+	// Factorial of negative numbers or values greater than 12 will overflow int
+	if nb < 0 || nb > 12 {
+		return 0
+	}
+
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result *= i
+		// Check for overflow
+		if result <= 0 {
 			return 0
 		}
-	
-		result := 1
-		for i := 1; i <= nb; i++ {
-			result *= i
-			// Check for overflow
-			if result <= 0 {
-				return 0
-			}
-		}
-	
-		return result
 	}
+
+	return result
+}
