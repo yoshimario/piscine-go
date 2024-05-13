e615777377a37c68ed4f3bcd493a29e732c2b2c7
skipina <paintballsean@gmail.com>
2024-05-13 15:26:01 +0300

    gofumpt fix

--- .history/Quest-04/iterativefactorial/piscine/iterativefactorial_20240513145238.go
+++ .history/Quest-04/iterativefactorial/piscine/iterativefactorial_20240513145238.go
@@ -1,5 +1,5 @@
-package piscine
 
+Copy code
 func IterativeFactorial(nb int64) int64 {
     // Factorial of negative numbers or values greater than 12 will overflow int64
     if nb < 0 || nb > 12 {
---
+++ .history/Quest-04/iterativefactorial/piscine/iterativefactorial_20240513145427.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "fmt"
+func main() {
+	arg := 13 // Example argument
+	// Convert int to int64
+	arg64 := int64(arg)
+	result := piscine.IterativeFactorial(arg64)
+	fmt.Printf("IterativeFactorial(%d) = %d\n", arg, result)
+
+	// Handling negative input case
+	negativeArg := -8552095782491927294
+	// Convert int to int64
+	negativeArg64 := int64(negativeArg)
+	negativeResult := piscine.IterativeFactorial(negativeArg64)
+	fmt.Printf("IterativeFactorial(%d) = %d\n", negativeArg, negativeResult)
+}
