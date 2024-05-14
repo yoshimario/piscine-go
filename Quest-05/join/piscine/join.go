da204f3c57a70643bbe07c31c768f2e447747ed7
skipina <paintballsean@gmail.com>
2024-05-14 20:13:49 +0300

    join

---
+++ Quest-05/join/piscine/join.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	result := ""
+	for i, str := range strs {
+		if i > 0 {
+			result += sep
+		}
+		result += str
+	}
+	return result
+}
