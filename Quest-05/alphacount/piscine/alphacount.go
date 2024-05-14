2c7915b661cf25b920756b24226449e13c25a9f1
skipina <paintballsean@gmail.com>
2024-05-14 12:58:18 +0300

    gofumpt error fix

--- Quest-05/alphacount/piscine/alphacount.go
+++ Quest-05/alphacount/piscine/alphacount.go
@@ -1,11 +1,11 @@
 package piscine
 
 func AlphaCount(s string) int {
-    count := 0
-    for _, char := range s {
-        if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
-            count++
-        }
-    }
-    return count
+	count := 0
+	for _, char := range s {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
+			count++
+		}
+	}
+	return count
 }
