008b2344d1257cf92d3cc943af789b02ab3b48a4
skipina <paintballsean@gmail.com>
2024-05-14 14:09:39 +0300

    fixed the tolower

--- Quest-05/tolower/piscine/tolower.go
+++ Quest-05/tolower/piscine/tolower.go
@@ -4,9 +4,9 @@ func ToLower(s string) string {
 	var result string
 	for _, char := range s {
 		if char >= 'A' && char <= 'Z' {
-			result += string(char + 32) // Convert lowercase to uppercase by subtracting 32 from ASCII value
+			result += string(char + 32) // Convert uppercase to lowercase by adding 32 to ASCII value
 		} else {
-			result += string(char) // Keep non-lowercase characters unchanged
+			result += string(char) // Keep non-uppercase characters unchanged
 		}
 	}
 	return result
