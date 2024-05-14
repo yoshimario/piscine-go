f932ddb846a6212b5009adf726cd42f2937c4e16
skipina <paintballsean@gmail.com>
2024-05-14 14:03:30 +0300

    added tolowerr changes

--- Quest-05/tolower/piscine/tolower.go
+++ Quest-05/tolower/piscine/tolower.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	var result string
 	for _, char := range s {
 		if char >= 'A' && char <= 'Z' {
-			result += string(char - 32) // Convert lowercase to uppercase by subtracting 32 from ASCII value
+			result += string(char + 32) // Convert lowercase to uppercase by subtracting 32 from ASCII value
 		} else {
 			result += string(char) // Keep non-lowercase characters unchanged
 		}
