e6249e49cdeca17bcf7a3ef0359df2e99fe1d9ba
skipina <paintballsean@gmail.com>
2024-05-14 13:50:09 +0300

    isprintable removed import unicode

--- isprintable.go
+++ isprintable.go
@@ -1,10 +1,8 @@
 package piscine
 
-import "unicode"
-
 func IsPrintable(s string) bool {
 	for _, char := range s {
-		if !unicode.IsPrint(char) {
+		if char < 32 || char > 126 {
 			return false
 		}
 	}
