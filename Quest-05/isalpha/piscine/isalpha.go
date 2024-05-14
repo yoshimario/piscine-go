1ceb2def0e9649b4086af3120f69f3ceac3afe13
skipina <paintballsean@gmail.com>
2024-05-14 13:19:38 +0300

    added a fix to is alpha

--- Quest-05/isalpha/piscine/isalpha.go
+++ Quest-05/isalpha/piscine/isalpha.go
@@ -4,15 +4,10 @@ func IsAlpha(s string) bool {
 	// Iterate over each character in the input string
 	for _, char := range s {
 		// Check if the character is not alphanumeric
-		if !isAlphaNumeric(char) {
+		if !((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')) {
 			return false
 		}
 	}
 	// If all characters are alphanumeric or the string is empty, return true
 	return true
-}
-
-// Helper function to check if a character is alphanumeric
-func isAlphaNumeric(char rune) bool {
-	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')
 }
