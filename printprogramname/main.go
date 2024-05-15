cb56ebbbc39b5191123076dce22b21587a26eb27
skipina <paintballsean@gmail.com>
2024-05-15 12:07:48 +0300

    gofumpt fix

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-    // Get the executable name
-    exe, err := os.Executable()
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
+	// Get the executable name
+	exe, err := os.Executable()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-    // Extract the base name from the path
-    name := filepath.Base(exe)
+	// Extract the base name from the path
+	name := filepath.Base(exe)
 
-    // Print the name of the executable
-    fmt.Println(name)
+	// Print the name of the executable
+	fmt.Println(name)
 }
