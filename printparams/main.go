4552950978de89cdbaad2ada368b54f91103b503
skipina <paintballsean@gmail.com>
2024-05-15 12:48:48 +0300

    added gofumpt

--- printparams/main.go
+++ printparams/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-    "os"
-    "github.com/01-edu/z01"
+	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-    // Iterate over the command line arguments starting from the first argument (ignoring the program name)
-    for _, arg := range os.Args[1:] {
-        // Iterate over each rune in the argument
-        for _, r := range arg {
-            z01.PrintRune(r)
-        }
-        // Print a newline after each argument
-        z01.PrintRune('\n')
-    }
+	// Iterate over the command line arguments starting from the first argument (ignoring the program name)
+	for _, arg := range os.Args[1:] {
+		// Iterate over each rune in the argument
+		for _, r := range arg {
+			z01.PrintRune(r)
+		}
+		// Print a newline after each argument
+		z01.PrintRune('\n')
+	}
 }
