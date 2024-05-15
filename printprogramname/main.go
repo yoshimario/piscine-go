58b71cb352ce6479efa024c8954033ddc41bd040
skipina <paintballsean@gmail.com>
2024-05-15 12:20:43 +0300

    changed function

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,8 +10,20 @@ func main() {
 	// Get the executable name using os.Args[0]
 	exe := os.Args[0]
 
+	// Find the last separator in the path
+	sep := '/'
+	lastSep := -1
+	for i, c := range exe {
+		if c == sep {
+			lastSep = i
+		}
+	}
+
 	// Extract the base name from the path
-	name := filepath.Base(exe)
+	name := exe
+	if lastSep != -1 {
+		name = exe[lastSep+1:]
+	}
 
 	// Print each character using z01.PrintRune
 	for _, r := range name {
