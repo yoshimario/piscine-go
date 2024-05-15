b5e0d4378fe514ada676339a16c2b103ab0e772b
skipina <paintballsean@gmail.com>
2024-05-15 12:05:52 +0300

    added printprogramname

---
+++ Quest-06/printprogramname/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+    // Get the executable name
+    exe, err := os.Executable()
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+    // Extract the base name from the path
+    name := filepath.Base(exe)
+
+    // Print the name of the executable
+    fmt.Println(name)
+}
