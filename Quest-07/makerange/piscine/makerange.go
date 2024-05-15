5fe664b65789b7181f8964ef5cbc8e2681ed90b6
skipina <paintballsean@gmail.com>
2024-05-16 02:27:06 +0300

    added files.

---
+++ Quest-07/makerange/piscine/makerange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	length := max - min
+	result := make([]int, length)
+	
+	for i := 0; i < length; i++ {
+		result[i] = min + i
+	}
+	return result
+}
