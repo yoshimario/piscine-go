e78d01d21c1c58bbf4eeea7f35aee5821a86a350
skipina <paintballsean@gmail.com>
2024-05-16 13:03:07 +0300

    added concatparams

---
+++ concatparams.go
@@ -0,0 +1,15 @@
+package piscine
+
+import "fmt"
+
+func ConcatParams(args []string) string {
+	var result string
+	for i, arg := range args {
+		if i == 0 {
+			result = arg
+		} else {
+			result = fmt.Sprintf("%s\n%s", result, arg)
+		}
+	}
+	return result
+}
