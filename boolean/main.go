56afc6f080d3b5da6571d6a7c6537c853143bcee
skipina <paintballsean@gmail.com>
2024-05-20 01:29:45 +0300

    added boolean

---
+++ boolean/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "github.com/01-edu/z01"
+    "os"
+)
+
+const (
+    EvenMsg = "I have an even number of arguments"
+    OddMsg  = "I have an odd number of arguments"
+)
+
+type boolean bool
+
+const (
+    yes boolean = true
+    no  boolean = false
+)
+
+func printStr(s string) {
+    for _, r := range s {
+        z01.PrintRune(r)
+    }
+    z01.PrintRune('\n')
+}
+
+func isEven(nbr int) boolean {
+    if nbr%2 == 0 {
+        return yes
+    } else {
+        return no
+    }
+}
+
+func main() {
+    lengthOfArg := len(os.Args[1:])
+    if isEven(lengthOfArg) == yes {
+        printStr(EvenMsg)
+    } else {
+        printStr(OddMsg)
+    }
+}
