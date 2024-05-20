25934bc3124f36648bc5bfa18cc95420715e69ca
skipina <paintballsean@gmail.com>
2024-05-20 11:43:48 +0300

    fixed gofumpt

--- boolean/main.go
+++ boolean/main.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-    "github.com/01-edu/z01"
-    "os"
+	"os"
+
+	"github.com/01-edu/z01"
 )
 
 const (
-    EvenMsg = "I have an even number of arguments"
-    OddMsg  = "I have an odd number of arguments"
+	EvenMsg = "I have an even number of arguments"
+	OddMsg  = "I have an odd number of arguments"
 )
 
 type boolean bool
 
 const (
-    yes boolean = true
-    no  boolean = false
+	yes boolean = true
+	no  boolean = false
 )
 
 func printStr(s string) {
-    for _, r := range s {
-        z01.PrintRune(r)
-    }
-    z01.PrintRune('\n')
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
 
 func isEven(nbr int) boolean {
-    if nbr%2 == 0 {
-        return yes
-    } else {
-        return no
-    }
+	if nbr%2 == 0 {
+		return yes
+	} else {
+		return no
+	}
 }
 
 func main() {
-    lengthOfArg := len(os.Args[1:])
-    if isEven(lengthOfArg) == yes {
-        printStr(EvenMsg)
-    } else {
-        printStr(OddMsg)
-    }
+	lengthOfArg := len(os.Args[1:])
+	if isEven(lengthOfArg) == yes {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
 }
