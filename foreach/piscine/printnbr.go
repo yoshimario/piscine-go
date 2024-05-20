1a0979b6a70f86b96de687d12fa9df5b39e9c9c6
skipina <paintballsean@gmail.com>
2024-05-21 00:15:00 +0300

    added new printnbr function

--- foreach/piscine/printnbr.go
+++ foreach/piscine/printnbr.go
@@ -2,18 +2,22 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+// PrintNbr prints an integer using z01.PrintRune.
 func PrintNbr(n int) {
-	if n == -9223372036854775808 {
-		z01.PrintRune('-')
-		z01.PrintRune('9')
-		n = 223372036854775808 // Add 1 to make it positive
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n *= -1
-	}
-	if n >= 10 {
-		PrintNbr(n / 10)
-	}
-	z01.PrintRune(rune('0' + n%10))
+    if n < 0 {
+        z01.PrintRune('-')
+        n = -n
+    }
+    if n == 0 {
+        z01.PrintRune('0')
+        return
+    }
+    var digits []rune
+    for n > 0 {
+        digits = append([]rune{rune(n%10 + '0')}, digits...)
+        n /= 10
+    }
+    for _, digit := range digits {
+        z01.PrintRune(digit)
+    }
 }
