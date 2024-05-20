b0888bd5a010e075238c6cd00157c7151dcb1b1e
skipina <paintballsean@gmail.com>
2024-05-20 18:37:04 +0300

    added ztail more stuff

--- ztail/main.go
+++ ztail/main.go
@@ -5,67 +5,82 @@ import (
 	"os"
 )
 
-func tailFile(fileName string, numBytes int) error {
-	file, err := os.Open(fileName)
+func parseNumChars(numChars string) int {
+	num := 0
+	for _, char := range numChars {
+		if char >= '0' && char <= '9' {
+			num = num*10 + int(char-'0')
+		} else {
+			break
+		}
+	}
+	return num
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func tailFile(filename, numChars string) error {
+	file, err := os.Open(filename)
 	if err != nil {
-		return fmt.Errorf("open %s: no such file or directory", fileName)
+		return fmt.Errorf("open %s: %v", filename, err)
 	}
 	defer file.Close()
 
-	stat, err := file.Stat()
+	fileStat, err := file.Stat()
 	if err != nil {
-		return fmt.Errorf("stat %s: %v", fileName, err)
+		return fmt.Errorf("stat %s: %v", filename, err)
 	}
 
-	offset := stat.Size() - int64(numBytes)
-	if offset < 0 {
-		offset = 0
+	fileSize := fileStat.Size()
+	num := parseNumChars(numChars)
+
+	if fileSize < int64(num) {
+		num = int(fileSize)
 	}
 
-	_, err = file.Seek(offset, 0)
+	buffer := make([]byte, num)
+	_, err = file.ReadAt(buffer, fileSize-int64(num))
 	if err != nil {
-		return fmt.Errorf("seek %s: %v", fileName, err)
+		return fmt.Errorf("read %s: %v", filename, err)
 	}
 
-	buf := make([]byte, numBytes)
-	n, err := file.Read(buf)
-	if err != nil {
-		return fmt.Errorf("read %s: %v", fileName, err)
-	}
-
-	fmt.Printf("==> %s <==\n", fileName)
-	fmt.Print(string(buf[:n]))
+	fmt.Print(string(buffer))
 	return nil
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) < 3 || args[0] != "-c" {
-		fmt.Println("Usage: go run . -c <num_bytes> file1.txt [file2.txt ...]")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run . -c <num> file1.txt [file2.txt ...]")
 		os.Exit(1)
 	}
 
-	numBytes := 0
-	if _, err := fmt.Sscanf(args[1], "%d", &numBytes); err != nil || numBytes <= 0 {
-		fmt.Println("Error: invalid number of bytes")
-		os.Exit(1)
+	numChars := os.Args[2]
+	filenames := os.Args[3:]
+
+	exitStatus := 0
+
+	for i, filename := range filenames {
+		if i == 0 && fileExists(filename) {
+			fmt.Printf("==> %s <==\n", filename)
+			if err := tailFile(filename, numChars); err != nil {
+				fmt.Println(err)
+				exitStatus = 1
+			}
+		} else if i != 0 && fileExists(filename) {
+			fmt.Println()
+			fmt.Printf("==> %s <==\n", filename)
+			if err := tailFile(filename, numChars); err != nil {
+				fmt.Println(err)
+				exitStatus = 1
+			}
+		} else if err := tailFile(filename, numChars); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			exitStatus = 1
+		}
 	}
 
-	exitCode := 0
-	errorsPrinted := 0
-
-	for i, fileName := range args[2:] {
-		err := tailFile(fileName, numBytes)
-		if err != nil {
-			fmt.Println(err)
-			errorsPrinted++
-			exitCode = 1
-		}
-
-		if i < len(args[2:])-1 {
-			fmt.Println() // Add a new line between files
-		}
-	}
-
-	os.Exit(exitCode)
+	os.Exit(exitStatus)
 }
