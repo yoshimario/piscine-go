9aefd98ad9a2797a6196846ca4a86766b2570e0f
skipina <paintballsean@gmail.com>
2024-05-20 11:52:26 +0300

    added printrune

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -12,8 +14,39 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func printInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	var digits []rune
+	for n > 0 {
+		digits = append([]rune{rune(n%10 + '0')}, digits...)
+		n /= 10
+	}
+	for _, digit := range digits {
+		z01.PrintRune(digit)
+	}
+}
+
 func main() {
 	points := &point{}
 	setPoint(points)
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.y)
+	z01.PrintRune('\n')
 }
