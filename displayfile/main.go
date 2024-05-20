b9e09261d154be6d4f67a4c2d4a8a3162b1fc63a
skipina <paintballsean@gmail.com>
2024-05-20 12:06:47 +0300

    added displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("File name missing")
+		return
+	}
+
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+
+	fileName := args[0]
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
