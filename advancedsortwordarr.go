bf2d917c95177fdeb7d67de9cf57f44d37b0d005
skipina <paintballsean@gmail.com>
2024-05-21 17:17:34 +0300

    added  new idea

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -1,19 +1,8 @@
 package piscine
 
-// AdvancedSortWordArr sorts a slice of strings based on the comparison function compareFunc.
-func AdvancedSortWordArr(strings []string, compareFunc func(a, b string) int) {
-	length := len(strings)
-	for i := 0; i < length; i++ {
-		for j := 0; j < length-i-1; j++ {
-			if compareFunc(strings[j], strings[j+1]) > 0 {
-				strings[j], strings[j+1] = strings[j+1], strings[j]
-			}
-		}
-	}
-}
-
-// Compare is an example comparison function to sort strings in ascending order by ASCII values.
-func Compare(a, b string) int {
+// CompareByASCII compares two strings by their ASCII values.
+// It returns a positive number if a > b, a negative number if a < b, and 0 if they are equal.
+func CompareByASCII(a, b string) int {
 	if a > b {
 		return 1
 	} else if a < b {
