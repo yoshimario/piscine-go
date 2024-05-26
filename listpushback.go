4aaf35594d06f5b0d1879c1629844f50dac98097
skipina <paintballsean@gmail.com>
2024-05-26 13:18:48 +0300

    added changes listpushback to root

---
+++ listpushback.go
@@ -0,0 +1,25 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+
+	if l.Tail == nil {
+		// If the list is empty, both Head and Tail should point to the new node
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		// Otherwise, add the new node at the end and update the Tail
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
