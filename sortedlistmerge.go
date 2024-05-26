85ca1978c6563b5651e69018e66080b659f21ee4
skipina <paintballsean@gmail.com>
2024-05-26 20:54:24 +0300

    added another change

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -36,3 +36,59 @@ func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	// Return the merged list, skipping the dummy node
 	return dummy.Next
 }
+
+func ListSort(l *NodeI) *NodeI {
+	if l == nil || l.Next == nil {
+		return l
+	}
+
+	// Split the list into two halves
+	mid := getMiddle(l)
+	nextToMid := mid.Next
+	mid.Next = nil
+
+	// Recursively sort the two halves
+	left := ListSort(l)
+	right := ListSort(nextToMid)
+
+	// Merge the sorted halves
+	sortedList := merge(left, right)
+	return sortedList
+}
+
+func getMiddle(head *NodeI) *NodeI {
+	if head == nil {
+		return head
+	}
+
+	slow := head
+	fast := head
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+func merge(left, right *NodeI) *NodeI {
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
+
+	var result *NodeI
+
+	if left.Data <= right.Data {
+		result = left
+		result.Next = merge(left.Next, right)
+	} else {
+		result = right
+		result.Next = merge(left, right.Next)
+	}
+
+	return result
+}
