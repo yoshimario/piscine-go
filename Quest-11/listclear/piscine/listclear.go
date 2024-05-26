9e2ab32166175d456a0efc419c322234752f4070
skipina <paintballsean@gmail.com>
2024-05-26 14:46:49 +0300

    added listclear

---
+++ Quest-11/listclear/piscine/listclear.go
@@ -0,0 +1,28 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+
+	if l.Tail == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
+
+func ListClear(l *List) {
+	l.Head = nil
+	l.Tail = nil
+}
--- listclear.go
+++ listclear.go
@@ -14,20 +14,15 @@ func ListPushBack(l *List, data interface{}) {
 	newNode := &NodeL{Data: data}
 
 	if l.Tail == nil {
-		// If the list is empty, both Head and Tail should point to the new node
 		l.Head = newNode
 		l.Tail = newNode
 	} else {
-		// Otherwise, add the new node at the end and update the Tail
 		l.Tail.Next = newNode
 		l.Tail = newNode
 	}
 }
 
-func ListLast(l *List) interface{} {
-	if l.Tail == nil {
-		// If the list is empty, return nil
-		return nil
-	}
-	return l.Tail.Data
+func ListClear(l *List) {
+	l.Head = nil
+	l.Tail = nil
 }
