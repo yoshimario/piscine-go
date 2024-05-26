0ccf05a90b24fe3065a04fd7c5882658e3809db6
skipina <paintballsean@gmail.com>
2024-05-26 19:36:29 +0300

    added listforeachif

---
+++ Quest-11/listforeachif/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func PrintElem(node *piscine.NodeL) {
+	fmt.Println(node.Data)
+}
+
+func StringToInt(node *piscine.NodeL) {
+	node.Data = 2
+}
+
+func PrintList(l *piscine.List) {
+	it := l.Head
+	for it != nil {
+		fmt.Print(it.Data, "->")
+		it = it.Next
+	}
+	fmt.Print("nil", "\n")
+}
+
+func main() {
+	link := &piscine.List{}
+
+	piscine.ListPushBack(link, 1)
+	piscine.ListPushBack(link, "hello")
+	piscine.ListPushBack(link, 3)
+	piscine.ListPushBack(link, "there")
+	piscine.ListPushBack(link, 23)
+	piscine.ListPushBack(link, "!")
+	piscine.ListPushBack(link, 54)
+
+	PrintList(link)
+
+	fmt.Println("--------function applied--------")
+	piscine.ListForEachIf(link, PrintElem, piscine.IsPositiveNode)
+
+	piscine.ListForEachIf(link, StringToInt, piscine.IsAlNode)
+
+	fmt.Println("--------function applied--------")
+	PrintList(link)
+
+	fmt.Println()
+}
