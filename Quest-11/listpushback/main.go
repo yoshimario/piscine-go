aadf91d1a1c73ac8b44af34be9b0b520ebddb174
skipina <paintballsean@gmail.com>
2024-05-26 12:59:44 +0300

    added listpushback

---
+++ Quest-11/listpushback/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+
+	link := &piscine.List{}
+
+	piscine.ListPushBack(link, "Hello")
+	piscine.ListPushBack(link, "man")
+	piscine.ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
+}
