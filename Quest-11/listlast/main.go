bd58c7697621915341583f1f92b7ea63b43f321d
skipina <paintballsean@gmail.com>
2024-05-26 13:23:24 +0300

    added listlast

---
+++ Quest-11/listlast/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	link := &piscine.List{}
+	link2 := &piscine.List{}
+
+	piscine.ListPushBack(link, "three")
+	piscine.ListPushBack(link, 3)
+	piscine.ListPushBack(link, "1")
+
+	fmt.Println(piscine.ListLast(link))
+	fmt.Println(piscine.ListLast(link2))
+}
