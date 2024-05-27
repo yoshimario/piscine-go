f4af1de326168e5a0ab35f778ab464b8d97409f1
skipina <paintballsean@gmail.com>
2024-05-27 14:12:53 +0300

    added fixes

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -1,44 +1,27 @@
 package piscine
 
-// BTreeDeleteNode deletes the given node from the tree.
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
 	if node == nil {
 		return root
 	}
-
-	if node.Left == nil && node.Right == nil { // Case 1: Leaf node
-		if node.Parent != nil {
-			if node == node.Parent.Left {
-				node.Parent.Left = nil
-			} else {
-				node.Parent.Right = nil
-			}
-		} else {
+	if node.Data < root.Data {
+		root.Left = BTreeDeleteNode(root.Left, node)
+	} else if node.Data > root.Data {
+		root.Right = BTreeDeleteNode(root.Right, node)
+	} else {
+		if root.Left == nil {
+			new := root.Right
 			root = nil
+			return new
+		} else if root.Right == nil {
+			new := root.Left
+			root = nil
+			return new
 		}
-	} else if node.Left == nil || node.Right == nil { // Case 2: Node has only one child
-		var child *TreeNode
-		if node.Left != nil {
-			child = node.Left
-		} else {
-			child = node.Right
-		}
-		if node.Parent != nil {
-			if node == node.Parent.Left {
-				node.Parent.Left = child
-			} else {
-				node.Parent.Right = child
-			}
-			child.Parent = node.Parent
-		} else {
-			root = child
-			child.Parent = nil
-		}
-	} else { // Case 3: Node has two children
-		successor := BTreeMin(node.Right)
-		node.Data = successor.Data
-		root = BTreeDeleteNode(root, successor)
-	}
+		new := BTreeMin(root.Right)
 
+		root.Data = new.Data
+		root.Right = BTreeDeleteNode(root.Right, new)
+	}
 	return root
 }
