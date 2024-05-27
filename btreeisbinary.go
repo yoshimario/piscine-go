0e56b2f633b994560a1f929eab2264feb49abeeb
skipina <paintballsean@gmail.com>
2024-05-27 11:06:03 +0300

    added gofumpt

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -4,51 +4,51 @@ import "strconv"
 
 // TreeNode represents a node in the binary tree
 type TreeNode struct {
-    Data  string
-    Left  *TreeNode
-    Right *TreeNode
+	Data  string
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 // BTreeIsBinary checks if a tree is a binary search tree
 func BTreeIsBinary(root *TreeNode) bool {
-    return isBST(root, nil, nil)
+	return isBST(root, nil, nil)
 }
 
 // isBST is a helper function to recursively check if a tree is a BST
 func isBST(node *TreeNode, min *int, max *int) bool {
-    if node == nil {
-        return true
-    }
-    
-    // Convert node.Data to an integer for comparison
-    nodeValue, err := strconv.Atoi(node.Data)
-    if err != nil {
-        return false
-    }
-    
-    // Check if the current node violates the min/max constraint
-    if (min != nil && nodeValue <= *min) || (max != nil && nodeValue >= *max) {
-        return false
-    }
-    
-    // Recursively check the left and right subtrees
-    return isBST(node.Left, min, &nodeValue) && isBST(node.Right, &nodeValue, max)
+	if node == nil {
+		return true
+	}
+
+	// Convert node.Data to an integer for comparison
+	nodeValue, err := strconv.Atoi(node.Data)
+	if err != nil {
+		return false
+	}
+
+	// Check if the current node violates the min/max constraint
+	if (min != nil && nodeValue <= *min) || (max != nil && nodeValue >= *max) {
+		return false
+	}
+
+	// Recursively check the left and right subtrees
+	return isBST(node.Left, min, &nodeValue) && isBST(node.Right, &nodeValue, max)
 }
 
 // BTreeInsertData inserts data into the binary search tree
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-    if root == nil {
-        return &TreeNode{Data: data}
-    }
-    
-    rootValue, _ := strconv.Atoi(root.Data)
-    dataValue, _ := strconv.Atoi(data)
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
 
-    if dataValue < rootValue {
-        root.Left = BTreeInsertData(root.Left, data)
-    } else {
-        root.Right = BTreeInsertData(root.Right, data)
-    }
-    
-    return root
+	rootValue, _ := strconv.Atoi(root.Data)
+	dataValue, _ := strconv.Atoi(data)
+
+	if dataValue < rootValue {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+
+	return root
 }
