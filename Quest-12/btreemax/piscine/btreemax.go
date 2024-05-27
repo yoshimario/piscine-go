d5b05288cd801c28aacb60b7f7bf1904695cbf77
skipina <paintballsean@gmail.com>
2024-05-27 13:54:52 +0300

    adde btreemax

---
+++ Quest-12/btreemax/piscine/btreemax.go
@@ -0,0 +1,13 @@
+package piscine
+
+// BTreeMax returns the node with the maximum value in the tree.
+func BTreeMax(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	current := root
+	for current.Right != nil {
+		current = current.Right
+	}
+	return current
+}
