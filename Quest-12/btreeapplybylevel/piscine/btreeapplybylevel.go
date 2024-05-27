85529a87c600dbe925ec6d163362f414f16d58c4
skipina <paintballsean@gmail.com>
2024-05-27 13:53:06 +0300

    added btreeapplybylevel fix

--- Quest-12/btreeapplybylevel/piscine/btreeapplybylevel.go
+++ Quest-12/btreeapplybylevel/piscine/btreeapplybylevel.go
@@ -1,29 +1,18 @@
 package piscine
 
-import "fmt"
-
-// BTreeApplyByLevel applies the function f to each node of the tree
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
 
-	// Create a queue to store nodes for level-order traversal
 	queue := []*TreeNode{root}
 
-	// Iterate through the queue until it's empty
 	for len(queue) > 0 {
-		// Pop the front node from the queue
 		node := queue[0]
 		queue = queue[1:]
 
-		// Apply the function f to the node's data
-		if _, err := f(node.Data); err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
+		_, _ = f(node.Data)
 
-		// Add the left and right children of the current node to the queue
 		if node.Left != nil {
 			queue = append(queue, node.Left)
 		}
