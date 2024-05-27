d769f7444a5bef2d7a146cedf889b9cf4923ec66
skipina <paintballsean@gmail.com>
2024-05-27 15:11:08 +0300

    fix

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -1,10 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 // BTreeDeleteNode deletes a node from the binary tree
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
 	dataToMove := []string{}
