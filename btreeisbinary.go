d826ab33aca81ee39062e9e928387194f3f5b067
skipina <paintballsean@gmail.com>
2024-05-27 11:04:32 +0300

    added btreebinary

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -1,47 +1,54 @@
 package piscine
 
+import "strconv"
+
+// TreeNode represents a node in the binary tree
 type TreeNode struct {
-	Left, Right *TreeNode
-	Data        string
+    Data  string
+    Left  *TreeNode
+    Right *TreeNode
 }
 
-// Insert data into the binary tree
-func BTreeInsertData(root *TreeNode, data string) {
-	if root == nil {
-		return
-	}
-
-	if data < root.Data {
-		if root.Left == nil {
-			root.Left = &TreeNode{Data: data}
-		} else {
-			BTreeInsertData(root.Left, data)
-		}
-	} else {
-		if root.Right == nil {
-			root.Right = &TreeNode{Data: data}
-		} else {
-			BTreeInsertData(root.Right, data)
-		}
-	}
-}
-
-// Helper function to check if the tree is a BST
-func isBSTHelper(node *TreeNode, min, max *TreeNode) bool {
-	if node == nil {
-		return true
-	}
-
-	// Check if the current node's data is within the valid range
-	if (min != nil && node.Data <= min.Data) || (max != nil && node.Data >= max.Data) {
-		return false
-	}
-
-	// Recursively check the left and right subtrees with updated ranges
-	return isBSTHelper(node.Left, min, node) && isBSTHelper(node.Right, node, max)
-}
-
-// Check if the binary tree is a BST
+// BTreeIsBinary checks if a tree is a binary search tree
 func BTreeIsBinary(root *TreeNode) bool {
-	return isBSTHelper(root, nil, nil)
+    return isBST(root, nil, nil)
+}
+
+// isBST is a helper function to recursively check if a tree is a BST
+func isBST(node *TreeNode, min *int, max *int) bool {
+    if node == nil {
+        return true
+    }
+    
+    // Convert node.Data to an integer for comparison
+    nodeValue, err := strconv.Atoi(node.Data)
+    if err != nil {
+        return false
+    }
+    
+    // Check if the current node violates the min/max constraint
+    if (min != nil && nodeValue <= *min) || (max != nil && nodeValue >= *max) {
+        return false
+    }
+    
+    // Recursively check the left and right subtrees
+    return isBST(node.Left, min, &nodeValue) && isBST(node.Right, &nodeValue, max)
+}
+
+// BTreeInsertData inserts data into the binary search tree
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+    if root == nil {
+        return &TreeNode{Data: data}
+    }
+    
+    rootValue, _ := strconv.Atoi(root.Data)
+    dataValue, _ := strconv.Atoi(data)
+
+    if dataValue < rootValue {
+        root.Left = BTreeInsertData(root.Left, data)
+    } else {
+        root.Right = BTreeInsertData(root.Right, data)
+    }
+    
+    return root
 }
