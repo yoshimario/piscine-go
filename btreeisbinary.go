be880c2432f71dd7a90dec020a611c00ebe64758
skipina <paintballsean@gmail.com>
2024-05-27 11:49:29 +0300

    more fix

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -25,18 +25,3 @@ func BTreeIsBinary(root *TreeNode) bool {
 	}
 	return true
 }
-
-// BTreeInsertData inserts data into the binary search tree
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
-	}
-
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
-	}
-
-	return root
-}
