66521b3881b8ce398a9d9c18d150275c7fe9d416
skipina <paintballsean@gmail.com>
2024-05-27 11:48:41 +0300

    more fix

--- Quest-12/btreeisbinary/piscine/btreeisbinary.go
+++ Quest-12/btreeisbinary/piscine/btreeisbinary.go
@@ -1,4 +1,13 @@
+// File: btreeisbinary.go
+
 package piscine
+
+// TreeNode represents a node in the binary tree
+type TreeNode struct {
+	Data  string
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 // BTreeIsBinary checks if a tree is a binary search tree
 func BTreeIsBinary(root *TreeNode) bool {
@@ -15,4 +24,19 @@ func BTreeIsBinary(root *TreeNode) bool {
 		return false
 	}
 	return true
+}
+
+// BTreeInsertData inserts data into the binary search tree
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+
+	return root
 }
